a8a603bf47ba97ea3ba5ee51e79a1236c7bcae2e
Aaron Fischer <fiscaa07@gmail.com>
2020-10-20 16:24:30 -0500

    Add project files.

---
+++ ArrangingCoins (LC 441)/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ArrangingCoins__LC_441_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numTextBox = new System.Windows.Forms.TextBox();
+            this.processButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numTextBox
+            // 
+            this.numTextBox.Location = new System.Drawing.Point(60, 29);
+            this.numTextBox.Name = "numTextBox";
+            this.numTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numTextBox.TabIndex = 0;
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(74, 65);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(75, 23);
+            this.processButton.TabIndex = 1;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 118);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.numTextBox);
+            this.Name = "Form1";
+            this.Text = "Arranging Coins";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numTextBox;
+        private System.Windows.Forms.Button processButton;
+    }
+}
+
---
+++ ArrangingCoins (LC 441)/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrangingCoins__LC_441_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            int userNum;
+            int count = 1;
+            int row = 1;
+
+            if (int.TryParse(numTextBox.Text, out userNum))
+            {
+                while (userNum - row > row)
+                {
+                    userNum -= 1;
+                    count++;
+                    row++;
+                }
+                
+                MessageBox.Show("The number of complete rows = " + count);
+                
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number.");
+            }
+        }
+    }
+}
